e459e99b5a5c5d9bc822fdb128d10777b6b5b95e
Michael Mosler-Krings <rmkrings@gmail.com>
2019-03-10 20:15:49 +0100

    Finished layout of settings view and started implementing view logic.

--- app/src/main/java/com/rmkrings/pius_app_for_android/SettingsActivity.java
+++ app/src/main/java/com/rmkrings/pius_app_for_android/SettingsActivity.java
@@ -1,15 +1,26 @@
 package com.rmkrings.pius_app_for_android;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.NumberPicker;
+import android.widget.EditText;
+import android.widget.Button;
 import com.rmkrings.helper.Config;
 
 public class SettingsActivity extends AppCompatActivity {
+    private EditText mUserName;
+    private EditText mPassword;
+    private Button mLoginButton;
+
+    private String sUserName = "";
+    private String sPassword = "";
 
     private void titlesForPicker(NumberPicker mPicker, String[] titles) {
-        Config config = new Config();
-
         mPicker.setMinValue(0);
         mPicker.setMaxValue(titles.length - 1);
         mPicker.setDisplayedValues(titles);
@@ -25,10 +36,60 @@ public class SettingsActivity extends AppCompatActivity {
         titlesForPicker((NumberPicker)findViewById(R.id.classPicker), config.getClasses());
     }
 
+    private void setLoginButtonState() {
+        boolean enabled = sUserName.length() > 0 && sPassword.length() > 0;
+        mLoginButton.setEnabled(enabled);
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        mUserName = findViewById(R.id.username);
+        mPassword = findViewById(R.id.password);
+        mLoginButton = findViewById(R.id.login);
+
+        // Check if login button must be enabled or disabled while typing on username or
+        // password field.
+        mUserName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                sUserName = mUserName.getText().toString();
+                setLoginButtonState();
+            }
+        });
+
+        mPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                sPassword = mPassword.getText().toString();
+                setLoginButtonState();
+            }
+        });
+
+        // Hide soft keyboard when login button clicked.
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mLoginButton.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(mPassword.getWindowToken(), 0);
+            }
+        });
 
         setTitle("Einstellungen");
         titlesForGradePicker();
