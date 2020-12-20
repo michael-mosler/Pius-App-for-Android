0b11b93728cba08db11df9d3ffd079466ab46f8c
Michael Mosler-Krings <rmkrings@gmail.com>
2020-12-20 17:03:53 +0100

    Fragments in Prefs did not show.

--- app/src/main/java/com/rmkrings/data/adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/rmkrings/data/adapter/ViewPagerAdapter.java
@@ -1,30 +1,36 @@
 package com.rmkrings.data.adapter;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentPagerAdapter;
 
-import com.rmkrings.fragments.preferences.AboutFragment;
-import com.rmkrings.fragments.preferences.GeneralFragment;
+import com.rmkrings.fragments.preferences.AboutPreferencesFragment;
+import com.rmkrings.fragments.preferences.GeneralPreferencesFragment;
 
-public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+public class ViewPagerAdapter extends FragmentPagerAdapter {
 
-    private static int TAB_COUNT = 2;
+    private static final int TAB_COUNT = 2;
+    private final GeneralPreferencesFragment mGeneralPreferencesFragment;
+    private final AboutPreferencesFragment mAboutPreferencesFragment;
 
     public ViewPagerAdapter(FragmentManager fm) {
-        super(fm);
+        super(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mGeneralPreferencesFragment = new GeneralPreferencesFragment();
+        mAboutPreferencesFragment = new AboutPreferencesFragment();
     }
 
+    @NonNull
     @Override
     public Fragment getItem(int position) {
-
         switch (position) {
             case 0:
-                return GeneralFragment.newInstance();
+                return mGeneralPreferencesFragment;
             case 1:
-                return AboutFragment.newInstance();
+                return mAboutPreferencesFragment;
+            default:
+                return new Fragment();
         }
-        return null;
     }
 
     @Override
@@ -36,9 +42,9 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
     public CharSequence getPageTitle(int position) {
         switch (position) {
             case 0:
-                return GeneralFragment.getTitle();
+                return mGeneralPreferencesFragment.getTitle();
             case 1:
-                return AboutFragment.getTitle();
+                return mAboutPreferencesFragment.getTitle();
 
         }
         return super.getPageTitle(position);
