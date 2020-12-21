790324f884a1ff3781845ad35f326d29ded20cc9
Henrik Steffens <Development@th3ph4nt0m.de>
2020-12-21 18:24:46 +0100

    fixed version name for new fragment

--- app/src/main/java/com/rmkrings/fragments/preferences/AboutPreferencesFragment.java
+++ app/src/main/java/com/rmkrings/fragments/preferences/AboutPreferencesFragment.java
@@ -1,10 +1,13 @@
 package com.rmkrings.fragments.preferences;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -12,10 +15,26 @@ import com.rmkrings.activities.R;
 import com.rmkrings.pius_app_for_android;
 
 public class AboutPreferencesFragment extends Fragment {
+    //Outlets
+    private TextView mVersion;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_preferences_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mVersion = view.findViewById(R.id.version);
+
+        try{
+            String version = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
+            mVersion.setText(getResources().getText(R.string.label_app_name).toString().replaceAll("%v", version));
+        } catch (PackageManager.NameNotFoundException ex){
+            ex.printStackTrace();
+            mVersion.setText(getResources().getText(R.string.label_app_name).toString().replaceAll("%v", "Dev"));
+        }
     }
 
     public String getTitle() {
